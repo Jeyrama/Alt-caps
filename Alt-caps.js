37a4f127e33c416ec2b397838b1610af77abc24e
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-17 21:02:20 -0700

    Update Alt-caps.js

--- Alt-caps.js
+++ Alt-caps.js
@@ -10,3 +10,9 @@ The input will be a lowercase string with no spaces.
 
 
 // Solution
+
+function capitalize(s){
+  const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
+  const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
+  return [even, odd];
+};
